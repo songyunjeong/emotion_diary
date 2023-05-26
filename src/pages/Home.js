db4f468166d7dd3bfb73802c31cd65989459501b
songyunjeong <yungunaa@naver.com>
2023-05-27 00:14:11 +0900

    header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,13 @@
 import Button from "../component/Button";
+import Header from "../component/Header";
 
 const Home = () => {
   return (
     <div>
-      <Button
-        text={"기본 버튼"}
-        onClick={() => {
-          alert("default button");
-        }}
-      />
-      <Button
-        type="positive"
-        text={"긍정 버튼"}
-        onClick={() => {
-          alert("positive button");
-        }}
-      />
-      <Button
-        type="negative"
-        text={"부정 버튼"}
-        onClick={() => {
-          alert("negative button");
-        }}
+      <Header
+        title={"Home"}
+        leftChild={<Button type="positive" text={"긍정 버튼"} />}
+        rightChild={<Button type="negative" text={"부정 버튼"} />}
       />
     </div>
   );
