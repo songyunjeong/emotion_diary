fe26a0c0aa8b89d98502aab4d638903d6b7555eb
songyunjeong <yungunaa@naver.com>
2023-05-29 20:44:27 +0900

    home header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,11 @@
-import Editor from "./Editor";
+import "./Header.css";
 
-const Header = () => {
+const Header = ({ title, leftChild, rightChild }) => {
   return (
     <div className="Header">
-      <Editor />
+      <div className="header_left">{leftChild}</div>
+      <div className="header_title">{title}</div>
+      <div className="header_right">{rightChild}</div>
     </div>
   );
 };
