cb5e3b741c612660ca611c156ad4fd31456cc928
songyunjeong <yungunaa@naver.com>
2023-05-30 23:37:29 +0900

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,12 @@ import Button from "../component/Button";
 import Header from "../component/Header";
 import { DiaryStateContext } from "../App";
 import { getMonthRangeByDate } from "../util";
+import DiaryList from "../component/DiaryList";
 
 const Home = () => {
   const data = useContext(DiaryStateContext);
   const [pivotDate, setPivotDate] = useState(new Date());
-  const [filteredDate, setFilteredData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
   const headerTitle = `${pivotDate.getFullYear()}년 
                        ${pivotDate.getMonth() + 1}월`;
 
@@ -39,6 +40,7 @@ const Home = () => {
         leftChild={<Button text={"<"} onClick={onDecreaseMonth} />}
         rightChild={<Button text={">"} onClick={onIncreaseMonth} />}
       />
+      <DiaryList data={filteredData} />
     </div>
   );
 };
