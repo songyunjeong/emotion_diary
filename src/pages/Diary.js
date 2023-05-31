b1b2bbdd366d6217ba22a3c12a217a40b5695660
songyunjeong <yungunaa@naver.com>
2023-05-31 23:55:36 +0900

    diary page

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,14 +1,42 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import useDiary from "../hooks/useDiary";
+import { getFormattedDate } from "../util";
+import Header from "../component/Header";
+import Button from "../component/Button";
+import Viewer from "../component/Viewer";
 
 const Diary = () => {
   const { id } = useParams();
+  const data = useDiary(id);
+  const navigate = useNavigate();
 
-  return (
-    <div>
-      <div>{id}번 일기</div>
-      <div>Diary page</div>
-    </div>
-  );
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
+  if (!data) {
+    return <div>일기를 불러오고 있습니다...</div>;
+  } else {
+    const { date, emotionId, content } = data;
+    const title = `${getFormattedDate(new Date(Number(date)))} 기록`;
+
+    return (
+      <div>
+        <Header
+          title={title}
+          leftChild={<Button text={"< 뒤로가기"} onClick={goBack} />}
+          rightChild={<Button text={"수정하기"} onClick={goEdit} />}
+        />
+        <div>{id}번 일기</div>
+        <div>Diary page</div>
+        <Viewer content={content} emotionId={emotionId} />
+      </div>
+    );
+  }
 };
 
 export default Diary;
