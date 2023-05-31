ab994d9ec3fbba0c6c3c40c8df366c1a1dd3b2ef
songyunjeong <yungunaa@naver.com>
2023-06-01 00:14:12 +0900

    new page

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,5 +1,33 @@
+import { useNavigate } from "react-router-dom";
+import Header from "../component/Header";
+import Button from "../component/Button";
+import Editor from "../component/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+
 const New = () => {
-  return <div>New page</div>;
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const onSubmit = (data) => {
+    const { date, content, emotionId } = data;
+    onCreate(date, content, emotionId);
+    navigate("/", { replace: true });
+  };
+
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로 가기"} onClick={goBack} />}
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default New;
